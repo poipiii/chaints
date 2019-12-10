03f468d98852e8a24b88be5e27355174940ad10f
loy juncheng <juncheng03@gmail.com>
2019-12-10 15:28:27 +0800

    test commit 2

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print('hello world')
 print("Yeet")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
