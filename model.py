dbd6a8b15d6b96f6d07c2b0059b35f1acf7d2311
Dian Sofia <193016m@mymail.nyp.edu.sg>
2020-01-01 15:41:35 +0800

    Started working on the delivery side, specifically added in function to split cart stuff and create class

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from passlib.hash import pbkdf2_sha256
 #user model - email,username,password,orders,payment,address
 #orders - item,price,quantity, order id
 class User_Model:
-    #initaliser of User_Model 
+    #initaliser of User_Model
     def __init__(self,user_email,username,user_password,user_firstname,user_lastname,user_role):
         self.__user_role=user_role
         self.__user_id = uuid.uuid4().hex
@@ -45,7 +45,7 @@ class User_Model:
             self.__owned_products = None
     def append_owned_p(self,productid):
         self.__owned_products.append(productid)
-    
+
     def delete_owned_p(self,productid):
         self.__owned_products.remove(productid)
     #User_Model Accessor
@@ -75,46 +75,46 @@ class User_Model:
 
     def get_owned_products(self):
         return self.__owned_products
-    
 
-    
+
+
     def __str__(self):
         return 'username: {} email: {} password:{} firstname:{} lastname:{} fullname:{} usr_id:{} usr_role:{} owned_p:{}'.format(self.get_username(),self.get_user_email(),self.get_user_password(),self.get_user_firstname(),self.get_user_lastname(),self.get_user_fullname()
         ,self.get_user_id(),self.get_user_role(),self.get_owned_products())
 
 
 def update_usr_owned_p(userid,productid):
-    db = shelve.open('database/user_database/user.db','c') 
+    db = shelve.open('database/user_database/user.db','c')
     if userid in db:
         user = db.get(userid)
         user.append_owned_p(productid)
         db[userid] = user
     else:
         pass
-    db.close() 
+    db.close()
 
 def get_user(userid):
     db = shelve.open('database/user_database/user.db','r')
     if userid in db.keys():
         user = db.get(userid)
-        db.close()   
+        db.close()
     return user
- 
+
 def update_user(user_obj):
     db = shelve.open('database/user_database/user.db','r')
     if user_obj.get_user_id() in db.keys():
         db[user_obj.get_user_id()] = user_obj
-        db.close()   
+        db.close()
 
 
 # def delete_db():
-#     db = shelve.open('database/user_database/user.db','c') 
-#     db.clear() 
-#     db.close() 
+#     db = shelve.open('database/user_database/user.db','c')
+#     db.clear()
+#     db.close()
 
 
 
-# test_usr = User_Model('qq@qmail.com','poipii','654321','P','I','B') 
+# test_usr = User_Model('qq@qmail.com','poipii','654321','P','I','B')
 # print(test_usr)
 
 # def print_user_db():
@@ -136,9 +136,9 @@ class Product_Model:
         self.__product_price = product_price
         self.__product_discount = product_discount
         self.__product_images = product_images
-        self.__product_catergory = product_catergory  
-        
-    #Product_Model Mutator 
+        self.__product_catergory = product_catergory
+
+    #Product_Model Mutator
     def set_product_id(self):
         self.__product_id = uuid.uuid4().hex #used for genterating another id if it exist in shelve
 
@@ -165,7 +165,7 @@ class Product_Model:
     def set_product_catergory(self,product_catergory):
         self.__product_catergory = product_catergory
 
-    #Product_Model Accessor 
+    #Product_Model Accessor
     def get_product_id(self):
         return self.__product_id
 
@@ -208,7 +208,7 @@ class Product_Model:
 def Add_New_Products(user_id,product_name,product_current_qty,product_desc,product_price,product_discount,product_catergory,product_images):
     try:
         db = shelve.open('database/product_database/product.db','c')
-        New_Product = Product_Model(product_name,product_current_qty,product_desc,product_price,product_discount,product_catergory,product_images)       
+        New_Product = Product_Model(product_name,product_current_qty,product_desc,product_price,product_discount,product_catergory,product_images)
         db[New_Product.get_product_id()] = New_Product
         update_usr_owned_p(user_id,New_Product.get_product_id())
         product_logging(user_id,'CREATE',New_Product.get_product_id(),New_Product)
@@ -217,8 +217,8 @@ def Add_New_Products(user_id,product_name,product_current_qty,product_desc,produ
         raise 'db file not found'
     except KeyError:
         raise ' key error in shelve'
-   
-    
+
+
     db.close()
 
 
@@ -256,15 +256,15 @@ def fetch_products_by_user(user_id):
         raise 'db file not found'
     except KeyError:
         raise ' key error in shelve'
-    
+
     db.close()
     return product_list
 
-#take in product id and return the specific product details 
+#take in product id and return the specific product details
 def get_product_by_id(product_id):
     try:
-       
-        db = shelve.open('database/product_database/product.db','r')      
+
+        db = shelve.open('database/product_database/product.db','r')
         product = db.get(product_id)
     except IOError:
         raise 'db file not found'
@@ -276,7 +276,7 @@ def get_product_by_id(product_id):
     return product
 
 
-#edit take in product id and all product fields and overwrite sand update product in db 
+#edit take in product id and all product fields and overwrite sand update product in db
 def Edit_Products(user_id,product_id,product_name,product_current_qty,product_desc,product_price,product_discount,product_catergory,product_images):
     try:
         user = get_user(user_id)
@@ -325,28 +325,28 @@ def delete_product_by_id(product_id,user_id):
 
 
 
-#product db test codes 
+#product db test codes
 #print all products in product db in command line
 # def test_print():
-#     db = shelve.open('database/product_database/product.db','c')  
+#     db = shelve.open('database/product_database/product.db','c')
 #     for i in db.values():
 #         print(i)
-      
-#     db.close() 
+
+#     db.close()
 
 # test_print()
 #USE WITH CAUTION DELETE THE WHOLE PRODUCT DB
 # def delete_db():
-#     db = shelve.open('database/product_database/product.db','c') 
-#     db.clear() 
-#     db.close() 
+#     db = shelve.open('database/product_database/product.db','c')
+#     db.clear()
+#     db.close()
 
 #USE WITH CAUTION CREATE 30 GREY SHIRT PRODUCT IN PRODUCT DB
 
 
 # for i in range(29):
 #     Add_New_Products('grey shirt',200,'very grey shirt',100,1,['male'],['mango-man-1156-4297221-1.jpg'])
-#do not touch 
+#do not touch
 # def Add_New_Products(product_name,product_current_qty,product_desc,product_price,product_discount,product_catergory,product_images):
 #     try:
 #         db = shelve.open('database/product_database/product.db','c')
@@ -363,10 +363,10 @@ def delete_product_by_id(product_id,user_id):
 #             New_Product.set_product_catergory(product_catergory)
 #             New_Product.set_product_images(product_images)
 #             db[New_Product.get_product_id()] = New_Product
-    
+
 #     except KeyError:
 #         raise ' key error in shelve'
-    
+
 #     db.close()
 
 
@@ -410,7 +410,7 @@ class product_logger:
         return datetime.fromtimestamp(self.__timestamp)
     def __str__(self):
         return 'activity: {},productid: {}, product_obj {},timestamp {},datetime {}'.format(self.get_p_activity(),self.get_product_id(),self.get_object(),self.get_timestamp(),self.get_timestamp_as_datetime())
-       
+
 
 
 #take in user id , product activity product id product _obj
@@ -432,10 +432,10 @@ def product_logging(userid,product_activity,product_id,product_obj):
 def get_product_log_by_id(user_id):
     db = shelve.open('database/logs_database/logs.db','r')
     all_logs = db.get('b831c6bd18ef4d10bf625cacb443dcde')
-    product_logs = all_logs.get_product_log_list() 
+    product_logs = all_logs.get_product_log_list()
     db.close()
     return product_logs
-        
+
 
 # def print_log():
 #     db = shelve.open('database/logs_database/logs.db','c')
@@ -445,34 +445,64 @@ def get_product_log_by_id(user_id):
 #     db.close()
 # print_log()
 
+#==========DELIVERY=============
 
-#delivery stuff
+#creating class for individual orders in one cart (INITIAL)
 
-#creating class for individual orders in one cart
 class indi_product_order:
-    def __init__(self,prodid,quantity):
+    def __init__(self,prodid,quantity,orderdate,sellerid):
         self.__productid=prodid
+        self.__sellerid=sellerid
         self.__quantity=quantity
-        self.__deliverystat="pending"
+        self.__deliverystat="Pending"
+        self.__order_date=orderdate
         self.__indi_orderid= uuid.uuid4().hex
+        #self.adding_to_db()
     def get_product_id(self):
         return self.__productid
     def get_quantity(self):
         return self.__quantity
     def get_deliverystat(self):
         return self.__deliverystat
+    def get_order_date(self):
+        return self.__order_date
+    def get_individual_orderid(self):
+        return self.__indi_orderid
+    def get_seller_id(self):
+        return self.__sellerid
+    def __str__(self):
+        return "prod id: %s, seller id: %s, quantity: %d, deliverystat: %s, date: %s, indi id: %s"%(self.get_product_id(),self.get_seller_id(),self.get_quantity(),self.get_deliverystat(),self.get_order_date(),self.get_individual_orderid())
+
+
+#function to add individual order ids into the delivery status database rip
+#def Adding_to_delivery_db(productid,quantity,deliverystatus,orderdate,orderid):
+
+
 
 #separating the orders in the order so that each item will have their own separate order id
-#sihui please use dictionary, have the product id be the key and the quantity be the value uwu
 #{'someid':3}
-def separating_orders(userid,userorders):
-    sepdict={}
+
+#========To Be Passed from Order to Delivery :(==========
+#          -customer id
+#          -Dictionary with orders(above ref)
+#          -Date of orders
+
+def separating_orders(customerid,sellerid,userorders,orderdate): #reminder: use jc's db to get seller using product
+    #<codes to open product db. The one above is temporary>
     somelist=[]
     for i in userorders:
-        indiproduct=indi_product_order(i,userorders[i])
+        #<code to find the seller id using product id to be passed to class add here>
+        indiproduct=indi_product_order(i,userorders[i],orderdate,sellerid)
+        print(indiproduct)
         somelist.append(indiproduct)
-    sepdict[userid]=somelist #i put in dictionary first, still thinking whether to create a separate db for this
-    return sepdict
+    try:
+        db = shelve.open('database/delivery_database/delivery.db','c')
+        db[customerid]=somelist
+        db.close()
+    except IOError:
+        raise Exception('db cannot be found')
+    except:
+        raise Exception("an unknown error has occurred ")
 
 
 
