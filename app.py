c17ecde71ee6688bc86408b4b2147f5279aa495f
loy juncheng <juncheng03@gmail.com>
2020-02-04 00:49:02 +0800

    atter techincal review 2

--- app.py
+++ app.py
@@ -598,7 +598,7 @@ def paymentdetails():
     payment1_form=Payment_Form(request.form)
     if request.method == "POST" and payment1_form.validate():
         if payment1_form.cardholder.data.isalpha():
-            payment_confirmation(payment1_form.cardholder.data,payment1_form.cardno.data,payment1_form.expiry.data,payment1_form.cvc.data,session.get('user_id'))
+            # payment_confirmation(payment1_form.cardholder.data,payment1_form.cardno.data,payment1_form.expiry.data,payment1_form.cvc.data,session.get('user_id'))
             return redirect(url_for('confirmation'))
         else:
             return render_template('Payment.html',form=payment1_form)
